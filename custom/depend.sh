91dac378da852541b0e61ea3e6ac27e5632b1037
Hovakim Grabski <hovakim.grabski@gmail.com>
2025-04-13 10:33:02 +0000

    lets try compiling version

--- custom/depend.sh
+++ custom/depend.sh
@@ -20,7 +20,15 @@ DEPS="\
     gpg \
     python3 \
     python3-dbus \
-    wget"
+    wget \
+    libcups2 cups \
+    libcups2-dev cups-bsd cups-client \
+    avahi-utils libavahi-client-dev libavahi-core-dev \
+    libavahi-common-dev libcupsimage2-dev libdbus-1-dev build-essential \
+    gtk2-engines-pixbuf ghostscript openssl libjpeg-dev libatk-adaptor libgail-common \
+    libsnmp-dev snmp-mibs-downloader libtool libtool-bin libusb-1.0-0-dev libusb-0.1-4 wget policykit-1 \
+    policykit-1-gnome automake1.11 python3-dbus.mainloop.pyqt5 python3-reportlab python3-notify2 python3-pyqt5 \
+    python3-dbus python3-gi python3-lxml python3-dev python3-pil python-is-python3 libsane libsane-dev sane-utils xsane"
 
 
 apt-get update
